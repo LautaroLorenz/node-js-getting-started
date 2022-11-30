00d677d1c951ef370ffdda656315831bc3a72f56
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-11-29 21:29:05 -0300

    menu options

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const cool = require('cool-ascii-faces');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require('cors');
+const app = express();
 
-express()
+if (process.env.DEV_MODE === 'true') {
+  app.use(cors({ origin: 'http://localhost:4200' }));
+}
+
+app
+  .get('/menu-options', (req, res) => res.json([{ value: 'option 1' }, { value: 'option 2' }]));
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 
 showTimes = () => {
   let result = '';
